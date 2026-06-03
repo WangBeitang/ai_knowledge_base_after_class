deb792ad6604552785f1165933b6df8e45764d1b
北堂 <2665468769@qq.com>
2026-06-03 23:30:36 +0800

    1.llm_providers新增嵌入模型 2.milvus_gateway

--- app/infra/llm/providers.py
+++ app/infra/llm/providers.py
@@ -1,19 +1,24 @@
 from app.infra.config.providers import infra_config
-from app.shared.model import get_llm_client
+from app.shared.model import get_llm_client,get_bge_m3_ef,generate_embeddings
 
 class LLMProvider:
+
     # 获取大语言模型
-    def chat(self, model_name:str=None, json_mode:bool=False):
-        return get_llm_client(model_name, json_mode)
+    def chat(self,model_name:str=None,json_mode:bool=False):
+        return get_llm_client(model_name,json_mode)
 
     # 获取视觉模型
-    def vision_chat(self, model_name:str=None):
+    def vision_chat(self,model_name:str=None):
         model_name = model_name or infra_config.llm.lv_model
-        return get_llm_client(model_name)
+        return get_llm_client(model=model_name)
+
+    # 获取嵌入模型
+    def embedding_mode(self):
+        return get_bge_m3_ef()
+
+    # 为文本列表生成稠密+稀疏混合向量嵌入
+    def embed_documents(self,documents:list[str]):
+        return generate_embeddings(documents)
+
 
 llm_provider = LLMProvider()
-
-if __name__ == "__main__":
-    llm = LLMProvider()
-    print(llm.chat())
-    print(llm.vision_chat())
