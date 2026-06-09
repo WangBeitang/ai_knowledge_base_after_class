9e3cec4acf25da96a11a775e64d40d1d12231da0
beitang <2665468769@qq.com>
2026-06-09 10:28:42 +0800

    node_item_name_confirm逻辑实现+测试

--- app/process/query/agent/nodes/node_item_name_confirm.py
+++ app/process/query/agent/nodes/node_item_name_confirm.py
@@ -19,3 +19,13 @@ def node_item_name_confirm(state):
     # 识别完成后写入完成列表，方便前端展示当前节点已结束。
     add_done_task(state["session_id"], sys._getframe().f_code.co_name, state["is_stream"])
     return state
+
+
+if __name__ == "__main__":
+    mock_state = {
+        "session_id": "test_session_001",
+        "original_query": "HAK 180 烫金机？",
+        "is_stream": False,
+    }
+    result_state = node_item_name_confirm(mock_state)
+    print(result_state)
--- app/rag/query/item_name_confirm_service.py
+++ app/rag/query/item_name_confirm_service.py
@@ -6,7 +6,8 @@ from app.infra.llm.providers import llm_provider
 from app.infra.persistence.history_repository import history_repository
 from app.infra.vectorstore.milvus_gateway import milvus_gateway
 from app.process.query.agent.state import QueryGraphState
-from app.rag.query.config import QUERY_HISTORY_LIMIT
+from app.rag.query.config import QUERY_HISTORY_LIMIT, ITEM_NAME_CONFIRM_THRESHOLD, ITEM_NAME_CANDIDATE_THRESHOLD, \
+    ITEM_NAME_OPTIONS_TOPK
 from app.shared.runtime.load_prompt import load_prompt
 from app.shared.runtime.logger import step_log,logger
 
@@ -46,6 +47,8 @@ def build_history_text(history_messages):
 
 
 def query_rewrite_and_item_name_recognition(original_query, history_text):
+    result_dict = {}
+
     # 1.获取llm客户端
     llm_client = llm_provider.chat(json_mode=True)
 
@@ -74,7 +77,56 @@ def search_item_name_in_milvus(item_names):
         dense_vector = embed_result["dense"][0]
         sparse_vector = embed_result["sparse"][0]
 
+        # 2. 创建搜索请求
         reqs = milvus_gateway.create_requests(dense_vector, sparse_vector,limit=10)
+
+        # 3.执行混合搜索
+        hybrid_result = milvus_gateway.hybrid_search(
+            collection_name=milvus_gateway.item_name_collection,
+            reqs=reqs,
+            ranker_weights=(0.6, 0.4),
+            limit=5,
+        )
+
+        # 4.搜索结果解析 [[{id, distance, entity:{item_name:}}]]
+        item_name_results = []
+        if hybrid_result and hybrid_result[0]:
+            for item in hybrid_result[0]:
+                score = item.get("distance", 0)
+                name = item.get("entity", {}).get("item_name", "")
+                item_name_results.append({"item_name": name, "score": score})
+
+        result_dict[item_name] = item_name_results
+        logger.info(f"商品[{item_name}]搜索结果：{item_name_results}")
+
+    return result_dict
+
+
+def classify_item_names(search_result_dict):
+    confirmed_list = []
+    candidate_list = []
+
+    # 数据格式：{item_name_llm: [{item_name:xxx,score:xx},{item_name:xxx,score:xx},...],item_name_llm: [{item_name:xxx,score:xx},{item_name:xxx,score:xx},...],...}
+    for item_name_llm, milvus_results in search_result_dict.items():
+        high_score_item_name_list = [
+            item_dict.get("item_name")
+            for item_dict in milvus_results
+            if item_dict.get("score") > ITEM_NAME_CONFIRM_THRESHOLD
+        ]
+
+        mid_score_item_name_list = [
+            item_dict.get("item_name")
+            for item_dict in milvus_results
+            if ITEM_NAME_CANDIDATE_THRESHOLD < item_dict.get("score") <= ITEM_NAME_CONFIRM_THRESHOLD
+        ]
+
+        if len(high_score_item_name_list) > 0:
+            confirmed_list.append(high_score_item_name_list[0])
+            continue
+        if len(mid_score_item_name_list) > 0:
+            candidate_list.extend(mid_score_item_name_list[:ITEM_NAME_OPTIONS_TOPK])
+
+    return confirmed_list, candidate_list
 
 
 def confirm_item_name(state: QueryGraphState) -> QueryGraphState:
@@ -102,9 +154,33 @@ def confirm_item_name(state: QueryGraphState) -> QueryGraphState:
     # 5. milvus 中查询和确认 item_name（前提：item_names 不为空）
     if item_names and len(item_names) > 0:
         # 5.1 在 milvus 中混合搜索每个 item_name
-        search_result_map = search_item_name_in_milvus(item_names)
+        search_result_dict = search_item_name_in_milvus(item_names)
 
         # 6. 根据评分分类：确认列表 / 可选列表
-        confirmed_list, candidate_list = classify_item_names(search_result_map)
+        confirmed_list, candidate_list = classify_item_names(search_result_dict)
 
-    return state
+        # 7. state 更新
+        if confirmed_list and len(confirmed_list) > 0:
+            state["item_names"] = confirmed_list
+            state["rewritten_query"] = rewritten_query
+            return  state
+        if candidate_list and len(candidate_list) > 0:
+            state["item_names"] = []
+            state["rewritten_query"] = rewritten_query
+            state["answer"] = f"请问您想咨询的是[{','.join(candidate_list)}]吗？"
+            return state
+        state["item_names"] = []
+        state["rewritten_query"] = rewritten_query
+        state["answer"] = "非常抱歉，没有找到匹配的答案，请重新提问。"
+        return state
+
+    # 8. 保留此次对话的历史记录
+    history_repository.save_message(
+        session_id=session_id,
+        role="user",
+        text=original_query,
+        rewritten_query=state.get("rewritten_query", original_query),
+        item_names=state.get("item_names", [])
+    )
+    return  state
+
